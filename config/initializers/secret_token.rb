47665f7902b9916c62c0efc055da596a5d5e7d7d
Karandeep Chawla <ikarandeep@gmail.com>
2013-03-18 00:15:53 -0500

    Intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kgathletics::Application.config.secret_token = 'f037adb65e9e3f23cfdc923cfd9ec3a1651cc7ccc0b5e7e76d5e7e5079f04d9aba9780368a6deffd2aca6a3b2b7f9623ee038918861819c98950ecde2a3eedc2'
